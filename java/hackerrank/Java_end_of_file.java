c84fd2387953d563f6d8e927e2927456f87f69db
yak-fumblepack <liang.mike.to@gmail.com>
2021-07-05 21:23:33 -0400

    add my solution for Hackerrank problem Java end of file

---
+++ java/hackerrank/Java_end_of_file.java
@@ -0,0 +1,60 @@
+package hackerrank;
+
+/*
+* Solution by https://github.com/yak-fumblepack
+*/
+
+import java.util.*;
+import java.io.*;
+
+public class Java_end_of_file {
+
+  static BufferedReader br;
+  static PrintWriter out;
+  static StringTokenizer st;
+
+  public static void main(String[] args) throws IOException {
+    Scanner sc = new Scanner(System.in);
+    // br = new BufferedReader(new InputStreamReader(System.in));
+    // out = new PrintWriter(new OutputStreamWriter(System.out));
+    // br = new BufferedReader(new FileReader("in.txt"));
+    // out = new PrintWriter(new FileWriter("out.txt"));
+
+    int i = 1;
+    while (sc.hasNextLine()) {
+      System.out.printf("%d %s", i, sc.nextLine());
+      System.out.println("");
+      i += 1;
+    }
+
+    // out.close();
+    sc.close();
+  }
+
+  static String next() throws IOException {
+    while (st == null || !st.hasMoreTokens())
+      st = new StringTokenizer(br.readLine().trim());
+    return st.nextToken();
+  }
+
+  static long readLong() throws IOException {
+    return Long.parseLong(next());
+  }
+
+  static int readInt() throws IOException {
+    return Integer.parseInt(next());
+  }
+
+  static double readDouble() throws IOException {
+    return Double.parseDouble(next());
+  }
+
+  static char readCharacter() throws IOException {
+    return next().charAt(0);
+  }
+
+  static String readLine() throws IOException {
+    return br.readLine().trim();
+  }
+
+}
