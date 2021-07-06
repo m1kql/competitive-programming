16c1447b7eaacaad5a09aafa72f8958d3bfb964e
yak-fumblepack <liang.mike.to@gmail.com>
2021-07-05 21:23:21 -0400

    add my solution for Hackerrank problem Java stdin stdout ii

---
+++ java/hackerrank/Java_stdin_stdout_ii.java
@@ -0,0 +1,62 @@
+package hackerrank;
+
+/*
+* Solution by https://github.com/yak-fumblepack
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class Java_stdin_stdout_ii {
+
+  static BufferedReader br;
+  static PrintWriter out;
+  static StringTokenizer st;
+
+  public static void main(String[] args) throws IOException {
+    Scanner sc = new Scanner(System.in);
+    // br = new BufferedReader(new InputStreamReader(System.in));
+    // out = new PrintWriter(new OutputStreamWriter(System.out));
+    // br = new BufferedReader(new FileReader("in.txt"));
+    // out = new PrintWriter(new FileWriter("out.txt"));
+
+    int i = sc.nextInt();
+    sc.nextLine();
+    double d = sc.nextDouble();
+    sc.nextLine();
+    String s = sc.nextLine();
+
+    System.out.println("String: " + s);
+    System.out.println("Double: " + d);
+    System.out.println("Int: " + i);
+
+    // out.close();
+    sc.close();
+  }
+
+  static String next() throws IOException {
+    while (st == null || !st.hasMoreTokens())
+      st = new StringTokenizer(br.readLine().trim());
+    return st.nextToken();
+  }
+
+  static long readLong() throws IOException {
+    return Long.parseLong(next());
+  }
+
+  static int readInt() throws IOException {
+    return Integer.parseInt(next());
+  }
+
+  static double readDouble() throws IOException {
+    return Double.parseDouble(next());
+  }
+
+  static char readCharacter() throws IOException {
+    return next().charAt(0);
+  }
+
+  static String readLine() throws IOException {
+    return br.readLine().trim();
+  }
+}
