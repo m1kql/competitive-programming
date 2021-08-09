5c687c3cdf88b69a8c95c4bfd113f83bb66f5e90
yak-fumblepack <liang.mike.to@gmail.com>
2021-08-09 15:48:05 -0400

    add my solution for Dmoj problem List minimum

---
+++ java/dmoj/List_minimum.java
@@ -0,0 +1,32 @@
+package dmoj;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class List_minimum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n;
+        n = sc.nextInt();
+        sc.nextLine();
+
+        ArrayList<Integer> array = new ArrayList<>(n);
+
+        int number = 0;
+
+        for (int i = 0; i < n; i++) {
+            number = sc.nextInt();
+            array.add(number);
+        }
+
+        for (int i = 0; i < n; i++) {
+            int smallestNumber = Collections.min(array);
+            System.out.println(smallestNumber);
+            array.remove(array.indexOf(smallestNumber));
+        }
+
+        sc.close();
+    }
+}
