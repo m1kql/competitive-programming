d01f3bf9cc196ba0e2e64a9af7366cd49afd4662
yak-fumblepack <liang.mike.to@gmail.com>
2021-08-25 22:10:26 -0400

    add my solution to Hackerrank problem Java exception handling try catch

---
+++ java/hackerrank/Java_exception_handling_try_catch.java
@@ -0,0 +1,36 @@
+package hackerrank;
+
+import java.util.Scanner;
+
+public class Java_exception_handling_try_catch {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int x = 0;
+        int y = 0;
+
+        try {
+            x = sc.nextInt();
+        } catch (Exception e) {
+            System.out.println("java.util.InputMismatchException");
+            return;
+        }
+        try {
+            y = sc.nextInt();
+        } catch (Exception e) {
+            System.out.println("java.util.InputMismatchException");
+            return;
+        }
+
+        try {
+            System.out.println(x/y);
+        } catch (Exception e) {
+            System.out.println("java.lang.ArithmeticException: / by zero");
+            return;
+        }
+
+    }
+
+}
