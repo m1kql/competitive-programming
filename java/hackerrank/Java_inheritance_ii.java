60f7be98c86f017840e519fd11091495eac41e5b
yak-fumblepack <liang.mike.to@gmail.com>
2021-08-25 22:47:15 -0400

    add my solution to Hackerrank problem Java inheritance ii

---
+++ java/hackerrank/Java_inheritance_ii.java
@@ -0,0 +1,38 @@
+package hackerrank;
+
+class Arithmetic {
+    int add(int a, int b) {
+        int ans = a + b;
+        return ans;
+    }
+}
+
+class Adder extends Java_inheritance_ii.Arithmetic {
+
+}
+
+public class Java_inheritance_ii {
+
+    static class Arithmetic {
+        int add(int a, int b) {
+            int ans = a + b;
+            return ans;
+        }
+    }
+
+    static class Adder extends Arithmetic {
+
+    }
+
+    public static void main(String[] args) {
+        // Create a new Adder object
+        Adder a = new Adder();
+
+        // Print the name of the superclass on a new line
+        System.out.println("My superclass is: " + a.getClass().getSuperclass().getName());
+
+        // Print the result of 3 calls to Adder's `add(int,int)` method as 3 space-separated integers:
+        System.out.print(a.add(10,32) + " " + a.add(10,3) + " " + a.add(10,10) + "\n");
+    }
+
+}
