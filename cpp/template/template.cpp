fcd2e8fa3b66ab47c18c4256ec30b878344762ec
Mike Liang <liang.mike.to@gmail.com>
2022-08-01 13:10:14 -0400

    create cpp folders

---
+++ cpp/template/template.cpp
@@ -0,0 +1,34 @@
+/*
+ * author: mike
+ * 
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template<typename T> T gcd(T a, T b){return(b?__gcd(a,b):a);} 
+template<typename T> T lcm(T a, T b){return(a*(b/gcd(a,b)));} 
+
+#define ll long long
+#define ld long double
+#define all(a) (a).begin(), (a).end()
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+
+
+
+// write code here
+void solve() {
+
+}
+
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	solve();
+}
